a759e9530b11c0654ef5b39824859a15e1893970
Marcus Kelly <mek413@gmail.com>
2019-11-09 12:40:54 -0600

    Updated models folders

---
+++ DevState/models/project.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+    var Project = sequelize.define("Project", {
+      name: {type: DataTypes.STRING, allowNull: false},
+      managerId: { type: Sequelize.INTEGER,
+        references: {
+          model: User,
+          key: 'id'
+        }
+      },
+      contributor: {type: DataTypes.STRING, allowNull: true},
+      tasksLeft: {type: DataTypes.INTEGER, defaultValue: 0},
+    });
+    Project.associate = function(models){
+      Project.hasMany(models.Task, {
+        onDelete: "cascade"
+      });
+      Project.belongsTo(models.User, {
+          foreignKey: {
+              allowNull: false
+          }
+      });
+    }
+    return Project;
+  };
